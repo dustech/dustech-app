5fceaed43d11529a5fe8f877a4a2e25d5cd4cb9c
Stefano Cerruti <stefano.cerruti@outlook.com>
2024-02-22 12:44:53 +0100

    Introduce responsive grid

---
+++ src/Dustech.App.Web/HexGrid/HexGridResponsiveViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dustech.App.Web.HexGrid;
+
+
+
+public class HexGridResponsiveViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(HexGridResponsiveViewComponentModel model)
+    {
+        return View(model);
+    } 
+}
+
+
+public record HexGridResponsiveViewComponentModel
+{
+    public HexGridResponsiveViewComponentModel(HexGridViewComponentModel hexGridModel)
+    {
+        ArgumentNullException.ThrowIfNull(hexGridModel);
+        HexGridModel = hexGridModel;
+
+    }
+    public HexGridViewComponentModel HexGridModel { get; init; }
+}
