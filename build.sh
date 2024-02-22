112349bf0f2efe18fcdd9eed5e1650f64875fbb8
Stefano Cerruti <stefano.cerruti@outlook.com>
2024-02-22 10:35:09 +0100

    Add footer social

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 npm run sass:build && \
 cp ./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js ./src/Dustech.App.Web/wwwroot/js/
+cp ./node_modules/bootstrap-icons/font/fonts/* ./src/Dustech.App.Web/wwwroot/styles/fonts/
