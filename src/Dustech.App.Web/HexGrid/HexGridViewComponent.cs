58ccecb0f05e33b338b500be08b6d1c1b51279da
Stefano Cerruti <stefano.cerruti@outlook.com>
2024-02-22 12:39:33 +0100

    Introduce grid system

---
+++ src/Dustech.App.Web/HexGrid/HexGridViewComponent.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dustech.App.Web.HexGrid;
+
+public class HexGridViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(
+        HexGridViewComponentModel model)
+    {
+        ArgumentNullException.ThrowIfNull(model);
+        
+        var rowLength = model.MaxRowLength;
+        var hexes = model.Elements
+            .Select(h => h with { Height = model.HexHeight }).ToList();
+
+        var margin = model.Margin;
+        ICollection<HexGridRowViewComponentModel> rows =
+            CutRowsTailRecursive(hexes, 0, rowLength, [], model.Inverted)
+                .Select((r, i) => new HexGridRowViewComponentModel(r)
+                {
+                    RowNumber = i,
+                    Margin = margin,
+                    Inverted = model.Inverted
+                })
+                .ToList();
+        ;
+
+        model = model with
+        {
+            Rows = rows
+        };
+
+        return View(model);
+    }
+
+
+    private static IEnumerable<IEnumerable<HexViewComponentModel>>
+        CutRowsTailRecursive(IReadOnlyCollection<HexViewComponentModel> hexes,
+            int i,
+            int rowLength,
+            ICollection<IEnumerable<HexViewComponentModel>> accumulator,
+            bool inverted)
+    {
+        if (hexes.Count == 0)
+        {
+            return accumulator;
+        }
+        var tunedRowLength = inverted switch
+        {
+            false => i % 2 == 0 ? rowLength : rowLength - 1,
+            true => i % 2 != 0 ? rowLength : rowLength - 1,
+        };
+        
+        var currentRow = hexes.Take(tunedRowLength);
+        accumulator.Add(currentRow);
+
+        var hsRest = hexes.Skip(tunedRowLength).ToList();
+        var index = i + 1;
+
+        return CutRowsTailRecursive(hsRest, index, rowLength, accumulator, inverted);
+    }
+}
+
+public record HexGridViewComponentModel
+{
+    public HexGridViewComponentModel(
+        IEnumerable<HexViewComponentModel> elements)
+    {
+        var hexViewComponentModels = elements.ToList();
+        if (hexViewComponentModels.Count == 0)
+        {
+            throw new ArgumentException("Elements collection cannot be empty.",
+                nameof(elements));
+        }
+
+        Elements = hexViewComponentModels;
+    }
+
+    public IEnumerable<HexViewComponentModel> Elements { get; init; }
+    public double CellHeight => HexHeight + Margin;
+    public int RowCount => Rows.Count;
+
+    public double ContainerHeight =>
+        // FirstRow
+        CellHeight +
+        // Other rows times vertical offset CellHeight 
+        (RowCount - 1) * CellHeight * 0.75;
+
+
+    public ICollection<HexGridRowViewComponentModel>
+        Rows { get; init; } =
+        [];
+
+    public int MaxRowLength { get; init; }
+    public double Margin { get; init; }
+    public double HexHeight { get; init; }
+    public string ContainerClass { get; init; } = "";
+
+    public bool Inverted { get; init; }
+};
---
+++ src/Dustech.App.Web/HexGrid/HexViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dustech.App.Web.HexGrid;
+
+public class HexViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(HexViewComponentModel model)
+    {
+
+        return View(model);
+    }
+}
+
+public record HexViewComponentModel
+{
+    const double HexAngle = 30;
+    const double HexRad = HexAngle * (Math.PI / 180);
+    public double TopOffSet { get; init; }
+    public double LeftOffSet { get; init; }
+    public string ColorContext { get; init; } = "primary";
+    public double Height { get; init; }
+
+    public double Width => Height * Math.Cos(HexRad);
+
+    public string Content { get; init; } = "";
+    public string Icon { get; init; } = "";
+    
+}
