a0042045b92cd07bf47f7faff894efef0c6cf685
Stefano Cerruti <stefano.cerruti@eng.it>
2024-02-23 16:40:35 +0100

    Add font dir create in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+# Check if directory exists
+if [ ! -d "./src/Dustech.App.Web/wwwroot/styles/fonts/" ]; then
+  # If not, create
+  mkdir -p ./src/Dustech.App.Web/wwwroot/styles/fonts/
+fi
+
+
 npm run sass:build && \
 cp ./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js ./src/Dustech.App.Web/wwwroot/js/
 cp ./node_modules/bootstrap-icons/font/fonts/* ./src/Dustech.App.Web/wwwroot/styles/fonts/
