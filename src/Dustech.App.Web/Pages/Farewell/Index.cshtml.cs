2f69ad93254960fcf579a6943355f7c397079d43
dustech <stefano.cerruti@outlook.com>
2024-03-07 15:51:52 +0100

    Add person image model

--- src/Dustech.App.Web/Pages/Farewell/Index.cshtml.cs
+++ src/Dustech.App.Web/Pages/Farewell/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Dustech.App.Web.Pages.Farewell;
 
 
---
+++ src/Dustech.App.Web/Pages/Shared/_PersonImage.cshtml.cs
@@ -0,0 +1,4 @@
+// ReSharper disable once CheckNamespace
+namespace Dustech.App.Web.Pages;
+
+public record PersonImage(string Src);
