079d6fb3f6091f4d3de48aa07e1f0070b01acd29
Stefano Cerruti <stefano.cerruti@eng.it>
2024-03-08 16:46:34 +0100

    Add site mask

--- src/Dustech.App.Web/wwwroot/js/site.js
+++ src/Dustech.App.Web/wwwroot/js/site.js
@@ -1,9 +1,30 @@
-﻿
-document.addEventListener('DOMContentLoaded', (event) => {
-    const video = document.getElementById('background-video');
-    if(video)
-    {
-        video.playbackRate = 0.5;
-    }
+﻿(
+    function() {
+        function hideMask(mask) {
+            setTimeout(() => {
+                mask.classList.add("hidden");
+            }, 300);
+        }
 
-});
+        document.addEventListener('DOMContentLoaded', (event) => {
+            const video = document.getElementById('background-video');
+            const siteMask = document.getElementById("site-mask");
+
+
+            if (video) {
+                video.addEventListener('loadeddata', function () {
+                    video.playbackRate = 0.5;
+                    if (siteMask) {
+                        hideMask(siteMask);
+                    }
+                });
+            }
+
+            if (!video && siteMask) {
+                hideMask(siteMask);
+            }
+
+        });
+    }
+)
+();
