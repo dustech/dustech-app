7d88c860df9aa4b3782db690ce467c3dc42f0bdb
dustech <stefano.cerruti@outlook.com>
2024-03-11 01:35:23 +0100

    Introduce Users DAL

---
+++ src/Dustech.App.DAL.Tests/Program.fs
@@ -0,0 +1,21 @@
+﻿
+open System
+open Dustech.App.DAL
+
+
+
+let pwd = Environment.GetEnvironmentVariable("PSLpwd")
+let usr = Environment.GetEnvironmentVariable("PSLusr")
+let host = Environment.GetEnvironmentVariable("PSLhost")
+let database = Environment.GetEnvironmentVariable("PSLdatabase")
+let storage = Users.openCon {
+        Username = usr
+        Password = pwd
+        Host = host
+        Database = database 
+    }
+
+
+storage.ForEach(fun u -> printfn $"{u.Name}")
+
+
--- src/Dustech.App.DAL/Users.fs
+++ src/Dustech.App.DAL/Users.fs
@@ -1,5 +1,73 @@
 ﻿namespace Dustech.App.DAL
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System.Collections.Immutable
+open System.Data
+open Npgsql
+
+[<AutoOpen>]
+module Handlers =
+    let connectionStateChange sender (e: StateChangeEventArgs) =
+        printfn $"State change from {e.OriginalState} to {e.CurrentState}."
+
+    let connectionInfoMessage sender (e: NpgsqlNoticeEventArgs) = printfn $"Info: {e.Notice}."
+
+module Users =
+    type User = { Name: string }
+    type ConnectionData = {
+        Username:string
+        Password:string
+        Host:string
+        Database:string
+    }
+    let openCon (cd:ConnectionData)=
+
+        let (builder: NpgsqlConnectionStringBuilder) = NpgsqlConnectionStringBuilder()
+        builder.Database <- cd.Database
+        builder.Timeout <- 10
+        //builder.SslMode <- SslMode.Require
+        builder.Host <- cd.Host
+        builder.Username <- cd.Username
+        builder.Password <- cd.Password
+        builder.PersistSecurityInfo <- false
+
+
+        let asyncConnectAndDisconnect () =
+            async {
+                let connection = new NpgsqlConnection(builder.ConnectionString)
+                connection.StateChange.AddHandler connectionStateChange
+                connection.Notice.AddHandler connectionInfoMessage
+                let mutable storage = ImmutableList<User>.Empty
+                try
+                    do! Async.AwaitTask(connection.OpenAsync())
+
+                    let command = connection.CreateCommand()
+                    command.CommandType <- CommandType.Text
+
+                    command.CommandText <-
+                        """
+                                            SELECT t.*
+                                            FROM usr."user" t
+                                            LIMIT 501
+                                        """
+
+                    use! reader = command.ExecuteReaderAsync() |> Async.AwaitTask
+                    
+
+                    let rec readRows() = async {                            
+                            let! hasRowToRead = reader.ReadAsync() |> Async.AwaitTask
+                            if hasRowToRead then     
+                                let! name = reader.GetFieldValueAsync<string>(reader.GetOrdinal("name")) |> Async.AwaitTask
+                                let myUser = { Name = name }
+                                storage <- storage.Add(myUser)
+                                return! readRows()
+                                           
+                        }
+                    do! readRows()
+                    do! Async.AwaitTask(connection.CloseAsync())
+                    return storage
+                with ex ->
+                    printfn $"ERROR: %s{ex.Message}"
+                    return storage
+            }
+
+        asyncConnectAndDisconnect () |> Async.RunSynchronously
